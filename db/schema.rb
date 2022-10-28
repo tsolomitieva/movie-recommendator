5d04090c4e0fb5940834848cb3f477427f63a296
tsolomitieva <tsolomitieva@gmail.com>
2022-10-28 16:07:49 +0300

    migrating for adding column rating

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_28_130009) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_28_130632) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_28_130009) do
     t.integer "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "rating"
   end
 
 end
