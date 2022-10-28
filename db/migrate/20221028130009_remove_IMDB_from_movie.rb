1331dbfec7941dc2d93ab807135509669a5f0a44
tsolomitieva <tsolomitieva@gmail.com>
2022-10-28 16:31:37 +0300

    renaming migration

--- db/migrate/20221028130009_remove_IMDB_from_movie.rb
+++ db/migrate/20221028130009_remove_IMDB_from_movie.rb
@@ -1,4 +1,4 @@
-class RemoveFieldNameFromMovie < ActiveRecord::Migration[7.0]
+class RemoveIMDBFromMovie < ActiveRecord::Migration[7.0]
   def change
     remove_column :movies, :IMDB_rating, :decimal
   end
