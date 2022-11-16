28c0387f38bd21a17345ae0650150a1e3abd4e5a
tsolomitieva <tsolomitieva@gmail.com>
2022-11-16 16:36:02 +0200

    Adding search by title to movies with ransack gem

--- Gemfile
+++ Gemfile
@@ -60,6 +60,8 @@ gem 'acts-as-taggable-on', '~> 9.0'
 
 gem "kaminari"
 
+gem "ransack"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,8 +4,8 @@ class MoviesController < ApplicationController
   
   # GET /movies or /movies.json
   def index
-    @movies = Movie.all.page params[:page]
-   
+    @q = Movie.ransack(params[:q])
+    @movies = @q.result(distinct: true).page params[:page]
   end
 
   # GET /movies/1 or /movies/1.json
