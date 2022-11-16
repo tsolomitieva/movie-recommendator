4b0ca44f0d56c53e460387fa028998cc67a79215
tsolomitieva <tsolomitieva@gmail.com>
2022-11-16 15:10:04 +0200

    Adding pagination to movies with kaminari

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,7 +4,8 @@ class MoviesController < ApplicationController
   
   # GET /movies or /movies.json
   def index
-    @movies = Movie.all
+    @movies = Movie.all.page params[:page]
+   
   end
 
   # GET /movies/1 or /movies/1.json
