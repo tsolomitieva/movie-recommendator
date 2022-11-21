27ce4538a048de996cc44c05002ea27ecd199ada
Panagiotis Spirou <panagspirou@gmail.com>
2022-11-21 15:26:27 +0200

    Fix: Render movie.poster on movies/index only when present

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,7 +4,7 @@ class MoviesController < ApplicationController
   
   # GET /movies or /movies.json
   def index
-    @movies = Movie.all
+    @movies = Movie.all.limit(10)
   end
 
   # GET /movies/1 or /movies/1.json
