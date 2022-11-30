85350c8a80ac3304103d2750650467340e8f24c9
tsolomitieva <tsolomitieva@gmail.com>
2022-11-30 15:39:51 +0200

    correcting movies_controller

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -51,5 +51,17 @@ class Admin::MoviesController < ApplicationController
             end
          end
     end
+
+    private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_movie
+      @movie = Movie.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def movie_params
+      params.require(:movie).permit(:poster, :title, :plot, :rating, :year, :poster, :tag_list, category_ids:[])
+    end
 end
 
